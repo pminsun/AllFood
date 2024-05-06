13cfee9cead7146b2907374280b694349686ef5c
minsun309 <pminsun309@gmail.com>
2024-05-06 20:45:26 +0900

    최초 커밋

---
+++ src/components/Footer.jsx
@@ -0,0 +1,4 @@
+import styles from "@/styles/Footer.module.css";
+export default function Footer() {
+  return <footer className={styles.footer}></footer>;
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import styles from "@/styles/Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul>
+          <li>
+            <Link href={"/"}>Nutrition</Link>
+          </li>
+          <li>
+            <Link href={"/"}>RECIPES</Link>
+          </li>
+        </ul>
+      </nav>
+      <h1>AllFood</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href={"/"}>Login</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Sign up</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
